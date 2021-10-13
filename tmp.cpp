7cd73bae76ef55d2d7eaf6def15d784cf561ba84
WebsterQR <50137330+WebsterQR@users.noreply.github.com>
2021-10-13 03:41:00 +0300

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -15,91 +15,81 @@ long get_pow(int a, int b){
     }
     return res;
 }
+
 int main()
 {
+    long BIG_ING = get_pow(2, 30);
     int n, k;
-    long x, y;
-    std::vector <long> a = {};
-    std::vector <long> a1 = {};
+    std::cin >> n >> k;
+    std::vector<long> a;
     int left_pointer = 0;
     int right_pointer = n - 1;
-    long BIG_ING = get_pow(2, 30);
-    std::cin >> n >> k;
+
     for (int i = 0; i < n; ++i){
-        int tmp;
+        long tmp;
         std::cin >> tmp;
         a.push_back(tmp);
-        a1.push_back(NULL);
     }
 
-    for (int i = 0; i < k; i += 1){
-        if (right_pointer == -1){
-            for (int j = 0; j < n; j += 1){
-                a[j] = a1[j];
-                a1[j] = NULL;
-            }
-            right_pointer = n - 1;
-            left_pointer = 0;
-        }
-        if (left_pointer >= 0){x = a[left_pointer];}
-        else {x = a1[left_pointer];}
+    if (n == 1){
+        std::cout << "0";
+    }
+    else{
+        for (int i = 0; i < k; i += 1){
+            long x = a[left_pointer];
+            long y = a[right_pointer];
+            if (x < y){
+                long new_value = mod(x+y, BIG_ING);
+                a[left_pointer] = new_value;
 
-        if (right_pointer >= 0) {y = a[right_pointer];}
-        else {y = a1[right_pointer];}
-
-        if (x < y){
-            if (left_pointer >= 0){
-                a[left_pointer] = NULL;
-                left_pointer += 1;
-            }
-            else{
-                a1[left_pointer] = NULL;
-                left_pointer += 1;
-            }
-            right_pointer += 1;
-            if (right_pointer >= 0){
-                if (right_pointer < a.size()){
-                    a[right_pointer] = mod(x+y, BIG_ING);
+                if (right_pointer == n - 1){
+                    right_pointer = 0;
                 }
                 else{
-                    a.push_back(mod(x+y, BIG_ING));
+                    right_pointer += 1;
+                }
+
+                if (left_pointer == n - 1){
+                    left_pointer = 0;
+                }
+                else{
+                    left_pointer += 1;
                 }
             }
             else{
-                a1[right_pointer] = mod(x+y, BIG_ING);
+                long new_value = mod(y-x, BIG_ING);
+                a[right_pointer] = new_value;
+
+                if (right_pointer == 0){
+                    right_pointer = n - 1;
+                }
+                else{
+                    right_pointer -= 1;
+                }
+
+                if (left_pointer == 0){
+                    left_pointer = n - 1;
+                }
+                else{
+                    left_pointer -= 1;
+                }
+            }
+        }
+        if (left_pointer < right_pointer){
+            for (int j = left_pointer; j < right_pointer; j += 1){
+                std::cout << a[j] << " ";
+            }
+            for (int j = right_pointer; j < n; j += 1){
+                std::cout << a[j] << " ";
             }
         }
         else{
-            if (right_pointer >= 0){
-                a[right_pointer] = NULL;
-                right_pointer -= 1;
+            for (int j = left_pointer; j < n; j += 1){
+                std::cout << a[j] << " ";
             }
-            else{
-                a1[right_pointer] = NULL;
-                right_pointer -= 1;
-            }
-            if (left_pointer > 0){
-                a[left_pointer - 1] = mod(y-x, BIG_ING);
-                left_pointer -= 1;
-            }
-            else{
-                if (left_pointer < 0){
-                    a1[left_pointer - 1] = (y - x) % BIG_ING;
-                    left_pointer -= 1;
-                }
-                else{
-                    a1[-1] = mod(y-x, BIG_ING);
-                    left_pointer = -1;
-                }
+            for (int j = 0; j < right_pointer + 1; j += 1){
+                std::cout << a[j] << " ";
             }
         }
-    }
-    std::cout << a.size();
-    std::cout << a1.size();
-    for (int i = 0; i < a1.size(); i += 1){
-        std::cout << a1[i] << " ";
-    }
-    for (int i = 0; i < a.size(); i += 1){
-        std::cout << a[i] << " ";
     }
 }
